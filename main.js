10ca9fcc583bf032113e9a7c95373f333d4577bd
Adnan Karim <adnan.karim@ucalgary.ca>
2021-12-01 22:04:50 -0700

    new website, under dev

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+var typing = new Typed (".animate",{
+      strings:['a Graduate Student at the University of Calgary under the supervision of Dr Ryo Suzuki', '\na soon to be Husband to my lovely fiance Maira Ejaz','Adnan Karim'],
+      //strings: ['Adnan Karim^1000\n `Adnan Karim` ^1000\n `Fetching from source...`'],
+      typeSpeed:10,
+      backSpeed:30,
+      loop:false,
+      onStart: function(self)
+      {
+      	var x = document.getElementById('trial')
+      	x.style.display = 'none';
+      },
+      onComplete: function(self) 
+      {
+
+		var x = document.getElementById("work")
+		x.style.visibility = "";
+		// setInterval(function(){ alert("Hello"); }, 3000)
+
+
+
+      }
+   });
+
+
+
+
